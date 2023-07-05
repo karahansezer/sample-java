1f5b0f70b097efaa2ba8bf80f5cd818572e0b3d6
karahansezer <karahansezer1@gmail.com>
2023-07-05 07:07:34 +0000

    Jenkinsfile changed

--- src/main/java/com/samplejavaapp/app/controller/HelloController.java
+++ src/main/java/com/samplejavaapp/app/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.samplejavaapp.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/api/foos")
     @ResponseBody
     public String getFoos(@RequestParam String val) {
         String result = "QueryStringValue: " + val;
-        System.out.println(result);
+        logger.info(result);
         return result;
     }
 }
+
